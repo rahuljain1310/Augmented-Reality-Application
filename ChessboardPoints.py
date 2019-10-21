0e9686c4d5871035e66f6bbeedb42eb7f407856a
rahuljain1310 <ee1170476@iitd.ac.in>
2019-10-21 14:46:24 +0530

    K calculated

--- ChessboardPoints.py
+++ ChessboardPoints.py
@@ -2,34 +2,45 @@ import numpy as np
 import cv2
 import glob
 
+def getGrayImage(fname,shape):
+	img = cv2.imread(fname)
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	gray = cv2.resize(gray, shape)
+	return gray
+
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((7*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('*.jpg')
+images = glob.glob('CalibrationImages/*.jpg')
+Shape = None
 
 for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
-    if ret == True:
-        objpoints.append(objp)
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        print(corners2.shape,objp.shape)
-        imgpoints.append(corners2)
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(2000)
+	gray = getGrayImage(fname,(1008,756))
+	Shape = gray.shape[::-1]
+	ret, corners = cv2.findChessboardCorners(gray, (7,7),None)
+	if ret == True:
+		print(fname)
+		objpoints.append(objp)
+		corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+		imgpoints.append(corners2)
+		# Draw and display the corners
+		img = cv2.drawChessboardCorners(gray, (7,7), corners2, ret )
+		cv2.imshow('img',gray)
+		cv2.waitKey(2000)
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-print(ret, mtx, dist, rvecs, tvecs)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, Shape, None,None)
+if ret:
+	print("Camera Matrix")
+	print(mtx)
+else:
+	print("No Solution Found")
+
 cv2.destroyAllWindows()
